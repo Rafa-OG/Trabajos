b5b4dcb511b343f1fe920dc31e6e4ab77bb3d1b9
Rafael Alejandro Dominguez Gomez <145386610+Rafa-OG@users.noreply.github.com>
2023-09-26 09:40:25 -0400

    %Ciclones.pl

--- Dengue Hemorragico.pl
+++ Dengue Hemorragico.pl
@@ -1,19 +1,20 @@
-% Preguntas
-malestar(X):-nl,write(X),write(' tiene malestar?[s/n]:') ,read(R), R=s.
-fiebre(X):-nl,write(X),write(' tiene fiebre?[s/n]:'),read(R), R=s.
-dolor_art_mus(X):-nl,write(X),
-    write(' tiene dolor muscular o en articulaciones?[s/n]: '), read(R),R=s.
-rash(X):-nl,write(X),write(' tiene rash [s/n]: '), read(R),R=s.
-hemorragia(X):-nl,write(' tiene hemorragia [s/n]: '), read(R),R=s.
+%preposiciones
 
-% Las Reglas
-dengue(X):- malestar(X), fiebre(X), rash(X), dolor_art_mus(pepe).
-dengue_hemorragico(X):- malestar(X), fiebre(X), dolor_art_mus(pepe), rash(X), hemorragia(X).
+prop(1,'viento>=74',p).
+prop(2,'viento<96',p).
+prop(3,'viento>=96',p).
+prop(4,'viento<111',p).
+prop(5,'viento>=111',p).
 
+%proposiciones inferibles
+prop(10,'depresionTropical',i).
+prop(20,'tormentaTropical',i).
+prop(30,'huracan',i).
 
-% El diagnostico
-diagnostico(X):-nl, write('Se investiga DENGUE'),dengue(X),
-             nl, write(X), write(' tiene sintomas de DENGUE.'),fail.
-diagnostico(X):-nl, write('Se investiga DENGUE HEMORRAGICO'), dengue_hemorragico(X),
-             nl, write(X),write(' tiene sospecha de DENGUE HEMORRAGICO.').
-diagnostico(X):-write(' NO SE LOGRO UN DIAGNOSTICO.').
+%Clases
+clase(1,clasificacion_ciclon,[10,20,30]).
+
+%Reglas
+regla(1,y,[1,2],10,0.8).
+regla(2,y,[3,4],20,0.8).
+regla(3,y,5,30,1.0).
